406ecef0ef27961321e43c170d2a0c2f4b33c9ba
Maike <maikonalexandre574@gmail.com>
2025-08-05 09:55:29 -0300

    fix: remove default url

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   MODE: z.enum(["development", "production", "test"]).default("development"),
-  VITE_API_URL: z.string().default("http://localhost:4000"),
+  VITE_API_URL: z.string().url(),
   VITE_SERVICE_VERSION: z.string().default("1.0.0"),
 });
 
